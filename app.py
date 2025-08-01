822a537dfa01c75308924846147162a9b2085a7e
borney11 <sidSidhant50@gmail.com>
2025-08-01 22:18:58 +0530

    app.py

--- app.py
+++ app.py
@@ -80,7 +80,8 @@ uploaded_file = st.file_uploader("Upload Resume (PDF)", type="pdf")
 if uploaded_file:
     st.info("Resume uploaded successfully!")
 
-    jobs_df = pd.read_csv("jobs.csv")
+   jobs_df = pd.read_csv("jobs.csv", on_bad_lines='skip', engine='python')
+
 
     # Predefined skill & tool sets based on jobs.csv columns
     # Extract unique skills and tools from jobs_df columns
